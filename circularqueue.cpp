6c356d36ea688f2795e727fbd863438d7ab34790
Pannntastic <pannntastic@gmail.com>
2024-05-30 17:30:48 +0700

    set front dan rear -1

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -4,5 +4,13 @@ using namespace std;
 class{
     int front, rear, max =5;
     int queue[5];
+
+    public:
+        queues(){
+            front = -1;
+            rear = -1;
+        }
+
+        
 };
 
